32a1072834fa2b1f35a696820362bc76297956d5
Jayce Azua <jayce.azua@gmail.com>
2019-08-09 15:49:03 -0700

    [wip] hiding the env file and other garbage

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	// prompt user to put in their zipcode
 	// deal with .env file for apikey
-
+	var string zipcode
 	// apiLink := "https://samples.openweathermap.org/data/2.5/forecast/hourly?zip=94102&appid=455b79f495c12591e03323c61a869ce5"
 	response, err := http.Get("https://samples.openweathermap.org/data/2.5/forecast/hourly?zip=33145&appid=455b79f495c12591e03323c61a869ce5")
 
@@ -21,7 +21,7 @@ func main() {
 	// City Name
 	// description
 	// main
-	
+
 	// after displaying the weather ask the user
 	// how do you feel?
 
