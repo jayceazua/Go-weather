1c52b224a5a221fc52d8bb7a2dcc67bbc2decd83
Jayce Azua <jayce.azua@gmail.com>
2019-08-09 15:13:00 -0700

    [wip] displayed data

--- main.go
+++ main.go
@@ -10,8 +10,20 @@ import (
 
 // Api key = 455b79f495c12591e03323c61a869ce5
 func main() {
+
+	// prompt user to put in their zipcode
+	// deal with .env file for apikey
+
 	// apiLink := "https://samples.openweathermap.org/data/2.5/forecast/hourly?zip=94102&appid=455b79f495c12591e03323c61a869ce5"
-	response, err := http.Get("https://samples.openweathermap.org/data/2.5/forecast/hourly?zip=94102&appid=455b79f495c12591e03323c61a869ce5")
+	response, err := http.Get("https://samples.openweathermap.org/data/2.5/forecast/hourly?zip=33145&appid=455b79f495c12591e03323c61a869ce5")
+
+	// display a nice print on the cli of the weather info
+	// City Name
+	// description
+	// main
+	
+	// after displaying the weather ask the user
+	// how do you feel?
 
 	if err != nil {
 		fmt.Print(err.Error())
